25e7334adfd2b3c35367e80c1a28abe1627a5dfb
Knackie <43431712+Knackie@users.noreply.github.com>
2024-11-05 23:40:18 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+    int width;
+    int height;
+    cin >> width >> height; cin.ignore();
+    for (int i = 0; i < height; i++) {
+        string row;
+        cin >> row; cin.ignore();
+    }
+}
